9087d6a61b535cb82d13141d3aa891e7ba220b3e
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2024-10-02 20:53:23 -0300

    atualização PetCard.jsx

--- src/components/PetCard.jsx
+++ src/components/PetCard.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const PetCard = ({ petName, imageUrl }) => {
+    const navigate = useNavigate();
+
+    const handleCardClick = () => {
+        // Aqui você pode verificar se o usuário está logado
+        const isLoggedIn = false; // Substitua por sua lógica de autenticação
+
+        if (isLoggedIn) {
+            // Se o usuário estiver logado, navegue para a página de detalhes do pet
+            navigate(`/detalhes/${petName}`); // Exemplo de rota
+        } else {
+            // Se não estiver logado, redirecione para a página de login
+            navigate('/LoginPage');
+        }
+    };
+
     return (
-        <div className="pet-card">
+        <div className="pet-card" onClick={handleCardClick}>
             <img src={imageUrl} alt={petName} className="pet-image" />
             <h4>{petName}</h4>
         </div>
