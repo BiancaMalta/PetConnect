d02a8c2c43ef2adf4f9dec67d6884d284d5a064a
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2024-10-02 20:45:25 -0300

    correção da pag home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Importe o useNavigate
-import '../styles/App.css'; // Certifique-se de que o CSS está corretamente estilizado
+import { useNavigate } from 'react-router-dom';
+import '../styles/App.css';
+import { useAuth } from '../context/AuthContext'; // Caminho correto para AuthContext
+
 
 const Home = () => {
-    const navigate = useNavigate(); // Crie a instância do navigate
+    const navigate = useNavigate();
+    const { isAuthenticated, userType } = useAuth(); // Utilize o contexto
 
     const irParaAdocao = () => {
-        navigate('/adotar'); // Navegue para a página de adoção
+        navigate('/adotar'); // Navega para a página de adoção
     };
 
     const irParaDoacao = () => {
-        navigate('/doar'); // Navegue para a página de doação
+        if (isAuthenticated) {
+            // Redireciona com base no tipo de usuário
+            if (userType === 'doador') {
+                navigate('/meus-cadastros'); // Redireciona doador para meus cadastros
+            } else if (userType === 'adotante') {
+                navigate('/meus-pedidos'); // Redireciona adotante para meus pedidos
+            }
+        } else {
+            navigate('/login'); // Redireciona para a página de login se não estiver autenticado
+        }
     };
 
     return (
         <div className="home-container">
-            <header className="home-header">
-                {/* Você pode adicionar um título ou logo aqui, se desejar */}
-            </header>
+            <header className="home-header"></header>
             <div className="home-content">
                 <p className="description">
                     Há milhares de pets ansiosos por encontrar um lar cheio de amor e carinho. Você pode fazer a diferença...
