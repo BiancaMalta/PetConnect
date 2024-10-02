a6ffa8e3598cf9c728b78962e91fa84c364dae9f
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2024-10-02 20:51:49 -0300

    adição do autentificador

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
+// src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importando o CSS do Bootstrap
 import 'bootstrap/dist/js/bootstrap.bundle.min'; // Importando o JS do Bootstrap
-
 import { BrowserRouter } from 'react-router-dom'; // Importando BrowserRouter
+import { AuthProvider } from './context/AuthContext'; // Importando o AuthProvider
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <AuthProvider>
+                <App />
+            </AuthProvider>
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
