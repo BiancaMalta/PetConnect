1d12c950ce76f06f170e40a91a5ba78779d2d3c5
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2024-10-02 20:51:10 -0300

    correção de rota

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,37 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import Adotar from './pages/Adotar';
 import Doar from './pages/Doar';
-import Chamados from './pages/Chamados';
-import Solicitações from './pages/Solicitações';
+import MeusPedidos from './pages/MeusPedidos'; 
 import Chat from './pages/Chat';
-import CadastrarAnimal from './pages/CadastrarAnimal';
 import PerfilUsuario from './pages/PerfilUsuario';
+import LoginPage from './pages/LoginPage';
+import MeusCadastros from './pages/MeusCadastros';
+import { AuthProvider } from './context/AuthContext'; // Importa o AuthProvider
 import './styles/App.css';
 
 const App = () => {
     const location = useLocation(); // Obter a localização atual
 
     return (
-        <>
-            {/* Renderiza o Header apenas se não estiver na página inicial */}
-            {location.pathname !== '/' && <Header />}
-            
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/adotar" element={<Adotar />} />
-                <Route path="/doar" element={<Doar />} />
-                <Route path="/chamados" element={<Chamados />} />
-                <Route path="/solicitacoes" element={<Solicitações />} />
-                <Route path="/chat" element={<Chat />} />
-                <Route path="/cadastrar-animal" element={<CadastrarAnimal />} />
-                <Route path="/perfil" element={<PerfilUsuario />} />
-            </Routes>
-            
-            <Footer />
-        </>
+        <AuthProvider> {/* Envolve o app com o AuthProvider */}
+            <>
+                {/* Renderiza o Header apenas se não estiver na página inicial */}
+                {location.pathname !== '/' && <Header />}
+                
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/adotar" element={<Adotar />} />
+                    <Route path="/doar" element={<Doar />} />
+                    <Route path="/meus-pedidos" element={<MeusPedidos />} />
+                    <Route path="/chat" element={<Chat />} />
+                    <Route path="/perfil" element={<PerfilUsuario />} />
+                    <Route path='/login' element={<LoginPage />} />
+                    <Route path="/meus-cadastros" element={<MeusCadastros />} />        
+                </Routes>
+                
+                <Footer />
+            </>
+        </AuthProvider>
     );
 };
 
