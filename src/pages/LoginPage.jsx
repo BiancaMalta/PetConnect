db49e1e51c858e80346e60562cbd8cba7143261f
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2024-10-03 11:30:48 -0300

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,32 +1,31 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext'; // Caminho correto para AuthContext
+import { useAuth } from '../context/AuthContext'; 
 
 const LoginPage = () => {
     const [isRegistered, setIsRegistered] = useState(true);
     const navigate = useNavigate();
-    const { setIsAuthenticated, setUserType } = useAuth(); // Utilize o contexto
+    const { setIsAuthenticated, setUserType } = useAuth(); 
 
     const handleLogin = (e) => {
-        e.preventDefault(); // Evita o comportamento padrão do formulário
+        e.preventDefault(); 
         // Simule a autenticação bem-sucedida
-        setIsAuthenticated(true); // Usuário autenticado
-        const userType = 'adotante'; // Substitua pela lógica de autenticação real
-        setUserType(userType); // Define o tipo de usuário
+        setIsAuthenticated(true); 
+        const userType = 'adotante'; 
+        setUserType(userType); 
 
         if (userType === 'doador') {
-            navigate('/meus-cadastros'); // Redireciona doador para meus cadastros
+            navigate('/meus-cadastros'); 
         } else {
-            navigate('/meus-pedidos'); // Redireciona adotante para meus pedidos
+            navigate('/meus-pedidos'); 
         }
     };
 
     const handleRegister = (e) => {
-        e.preventDefault(); // Evita o comportamento padrão do formulário
-        // Lógica de registro (criação de conta etc.)
-        setIsAuthenticated(true); // Após registro, o usuário é autenticado
-        setUserType('doador'); // Defina como doador por padrão, ou ajuste conforme necessário
-        navigate('/meus-cadastros'); // Após registro, redireciona para meus cadastros
+        e.preventDefault(); 
+        setIsAuthenticated(true); 
+        setUserType('doador'); 
+        navigate('/meus-cadastros'); 
     };
 
     return (
