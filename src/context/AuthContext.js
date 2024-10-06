de14f1b850736b944a50524a6c2d065199367d53
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2024-10-06 11:38:55 -0300

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,34 +1,25 @@
 import React, { createContext, useContext, useState } from 'react';
 
-// Cria o contexto
 const AuthContext = createContext();
 
-// Provedor de autenticação
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null); // Estado do usuário
-    const [isAuthenticated, setIsAuthenticated] = useState(false); // Estado de autenticação
-    const [userType, setUserType] = useState(null); // Tipo de usuário
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [userType, setUserType] = useState(null); // Armazena o tipo de usuário
 
-    const login = (userData) => {
-        setUser(userData); // Define o usuário autenticado
-        setIsAuthenticated(true); // Marca como autenticado
-        setUserType(userData.type); // Define o tipo do usuário
-    };
-
+    const login = () => setIsAuthenticated(true);
     const logout = () => {
-        setUser(null); // Remove o usuário autenticado
-        setIsAuthenticated(false); // Marca como não autenticado
-        setUserType(null); // Limpa o tipo de usuário
+        setIsAuthenticated(false);
+        setUserType(null); // Reseta o tipo de usuário ao deslogar
     };
 
     return (
-        <AuthContext.Provider value={{ user, isAuthenticated, userType, setIsAuthenticated, setUserType, login, logout }}>
+        <AuthContext.Provider value={{ isAuthenticated, login, logout, setIsAuthenticated, userType, setUserType }}>
             {children}
         </AuthContext.Provider>
     );
 };
 
-// Hook para usar o contexto de autenticação
 export const useAuth = () => {
     return useContext(AuthContext);
 };
+
