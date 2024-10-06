97afbe1e0cace95f5f14cd24ffdc7881ae73e0ee
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2024-10-06 11:36:32 -0300

    adição do css

--- src/pages/PerfilUsuario.jsx
+++ src/pages/PerfilUsuario.jsx
@@ -1,26 +1,35 @@
-// src/components/PerfilUsuario.jsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Header from '../components/Header';
+import '../styles/PerfilUsuario.css'; // Importa o CSS
 
 const PerfilUsuario = () => {
+    const [usuario, setUsuario] = useState({});
+
+    useEffect(() => {
+        const usuarioData = localStorage.getItem('usuario');
+        if (usuarioData) {
+            setUsuario(JSON.parse(usuarioData));
+        }
+    }, []);
+
     return (
-        <div className="page-background">
-            <div className="page-background">
-                <h1>Perfil do Usuário</h1>
-                <form>
-                    <label>
-                        Nome:
-                        <input type="text" name="nome" required />
-                    </label>
-                    <label>
-                        Email:
-                        <input type="email" name="email" required />
-                    </label>
-                    <label>
-                        Telefone:
-                        <input type="tel" name="telefone" required />
-                    </label>
-                    <button type="submit" className="main-button">Atualizar</button>
-                </form>
+        <div>
+            <Header /> {/* Adiciona o Header */}
+            <h1>Perfil do Usuário</h1>
+            <div className="perfil-container">
+                <div className="perfil-foto">
+                    {usuario.foto ? (
+                        <img src={usuario.foto} alt="Foto de Perfil" className="foto-perfil" />
+                    ) : (
+                        <div className="foto-placeholder">Sem Foto</div>
+                    )}
+                </div>
+                <div className="perfil-info">
+                    <p><strong>Nome:</strong> {usuario.nome} {usuario.sobrenome}</p>
+                    <p><strong>Email:</strong> {usuario.email}</p>
+                    <p><strong>Contato:</strong> {usuario.contato}</p>
+                    <p><strong>Sobre:</strong> {usuario.sobre}</p>
+                </div>
             </div>
         </div>
     );
